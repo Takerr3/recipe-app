193e00c181668de12b1df72196f0bb927bac106e
Tristan Kerr <Tristan.Kerr@hotmail.com>
2020-01-10 08:00:25 -0500

    Updates

---
+++ precache-manifest.4d0079cce40ead9b485cfc1e49d194f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2262c6bb873a23c82a461fbc118ebdaa",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "ba2bcfd98116e17867d2",
+    "url": "/recipe-app/static/css/main.fbb91733.chunk.css"
+  },
+  {
+    "revision": "5acdfeffef6723721c04",
+    "url": "/recipe-app/static/js/2.8db79a21.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/recipe-app/static/js/2.8db79a21.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba2bcfd98116e17867d2",
+    "url": "/recipe-app/static/js/main.9568b56d.chunk.js"
+  },
+  {
+    "revision": "587a2710861430a91a63",
+    "url": "/recipe-app/static/js/runtime-main.1822fcf3.js"
+  }
+]);
